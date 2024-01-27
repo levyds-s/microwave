6a0dd77d609254bbd52e825f7ac7e4bd376423ce
levyds <122990579+levyds-s@users.noreply.github.com>
2024-01-27 12:56:55 -0300

    Add files via upload

--- microwave_screen.ino
+++ microwave_screen.ino
@@ -1,7 +1,5 @@
 #include <LiquidCrystal.h>
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -14,17 +12,15 @@ int buttonState_2 = 0;
 int led = 8;
 int cont = 0;
 int visual = 0;
-int cont_3 = 0;
+int visual_2 = 0;
+
 void setup() {
   pinMode(button_1,INPUT);
   pinMode(button_2,INPUT);
   pinMode(led,OUTPUT);
   Serial.begin(9600);
-  // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
 }
-
 void loop() {
   buttonState_1 = digitalRead(button_1);
   buttonState_2 = digitalRead(button_2);
@@ -35,32 +31,26 @@ void loop() {
     visual+=30;
     delay(300);
     if (visual > 30){
-      visual = 0;
-  
+      visual = 0;  
     }
   } 
   else if (buttonState_2 == HIGH){ 
     cont+=1;
     delay(500);
-  
   }
   else if (cont == 1){
     if (visual == 0 && minutes > 0){
       visual=59;
-    
     } 
     else if (minutes >=0){
       if (visual!=0){
         secunds-=1;
         visual -=1;
-        delay(1000);
-
+        visual_2 -=1;
+        delay(500);
       }
       else if (visual == 0){
         digitalWrite(led,HIGH);
-
-
-
       }
     }
   }
@@ -75,6 +65,9 @@ void loop() {
   // print the number of seconds since reset:
   lcd.print(minutes);
   lcd.print(":");
+  if(visual < 10){
+    lcd.print("0");
+  }
   lcd.print(visual);
 
   Serial.println(visual);
